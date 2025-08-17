fa0e6a612b6a89fe19633e9df93883dd86a071a9
emark-cloud <eakinleye97dami@gmail.com>
2025-08-17 15:54:32 +0100

    Update and rename PriceDropTrap.sol to MintSpikeResponse.sol

---
+++ src/MintSpikeResponse.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {DroseraResponse} from "drosera-contracts/response/DroseraResponse.sol";
+
+contract MintSpikeResponse is DroseraResponse {
+    struct MintEvent {
+        address to;
+        uint256 amount;
+        uint256 newTotalSupply;
+    }
+
+    MintEvent public lastEvent;
+
+    function decode(bytes calldata data) external override {
+        (address to, uint256 amount, uint256 newTotalSupply) =
+            abi.decode(data, (address, uint256, uint256));
+
+        lastEvent = MintEvent(to, amount, newTotalSupply);
+    }
+}
